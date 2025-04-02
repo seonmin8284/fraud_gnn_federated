4984d2b536883c68a4b8165fcb91b0f7498378ea
seonmin8284 <seonmin8284@naver.com>
2025-04-02 16:24:06 +0900

    modified:   federated/client_main.py

--- federated/client_main.py
+++ federated/client_main.py
@@ -1,16 +1,23 @@
 # federated/client_main.py
 
 import argparse
-from flwr.client import ClientApp
-from federated.client import client_fn
+import flwr as fl
+from flwr.common import Context
+from federated.client import client_app
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cid", type=str, required=True)
     args = parser.parse_args()
 
-    app = ClientApp(lambda: client_fn(args.cid))
-    app.run()
+    # node_config 안에 partition_id 포함시켜서 context로 전달
+    fl.client.start_client(
+        client=client_app,
+        server_address="localhost:8080",
+        config={"node_config": {"partition_id": args.cid}},
+    )
+
 
 if __name__ == "__main__":
     main()
