2671df57b9ca8c7053a9229890190bab6fbc7b76
seonmin8284 <seonmin8284@naver.com>
2025-04-02 15:07:06 +0900

    modified:   federated/server.py

--- federated/server.py
+++ federated/server.py
@@ -11,8 +11,8 @@ def start_server(server_address="0.0.0.0:8080", num_rounds=3):
         fraction_fit=1.0,             # 전체 클라이언트 참여
         fraction_evaluate=1.0,            # 전체 클라이언트 평가
         min_fit_clients=2,            # 최소 학습 클라이언트 수
-        min_evaluate_clients=2,           # 최소 평가 클라이언트 수
-        min_available_clients=2       # 전체 최소 연결 클라이언트 수
+        min_evaluate_clients=1,           # 최소 평가 클라이언트 수
+        min_available_clients=1       # 전체 최소 연결 클라이언트 수
     )
 
     fl.server.start_server(
