8ab0f8b348e39790833ec0f6c32148a0ae4e8e57
seonmin8284 <seonmin8284@naver.com>
2025-04-02 16:03:20 +0900

    modified:   federated/server.py

--- federated/server.py
+++ federated/server.py
@@ -1,6 +1,6 @@
 # federated/server.py
 
-from flwr.server.supernode.api import start_supernode_server
+from flwr.server import Server
 from flwr.server.strategy import FedAvg
 
 def start_server(server_address="0.0.0.0:9091", num_rounds=3):
@@ -15,7 +15,7 @@ def start_server(server_address="0.0.0.0:9091", num_rounds=3):
         min_available_clients=1,
     )
 
-    start_supernode_server(
+    Server(
         server_address=server_address,
         config={"num_rounds": num_rounds},
         strategy=strategy,
